9b585682494ab8213805435ff107644ee58e75d7
Ilya Yalymov <chezeee@gmail.com>
2023-01-22 13:57:54 +0300

    The event listener has been updated. Done

--- js/index.js
+++ js/index.js
@@ -2,9 +2,15 @@
 // Каждый раз, когда вы нажимаете кнопку **Start game**, она вызывает функции `startGame()` и `logAnalytics()`. Обновите прослушиватель событий, чтобы он вызывал `startGame()` только один раз, но продолжал вызывать `logAnalytics()` каждый раз. Вывод должен быть в консоли.
 
 const button = document.querySelector("#app-button");
+let isGameStarted = false;
 
 button.addEventListener("click", function () {
+  if (isGameStarted) {
+    return logAnalytics();
+  }
+
   startGame();
+  isGameStarted = true;
   logAnalytics();
 });
 
